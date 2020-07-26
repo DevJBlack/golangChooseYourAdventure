f294986617d9b403ab7a62d6b2d2ad6dfc1e7615
Devin Black <Devinjamesblack@gmail.com>
2020-07-26 11:01:07 -0600

    adding a port and a newhandler to see the text in a web application

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -4,10 +4,13 @@ import (
 	"chooseYourAdventure/cyoa"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA eb application on")
 	filename := flag.String("file", "gopher.json", "the JSON file with CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
@@ -23,5 +26,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
