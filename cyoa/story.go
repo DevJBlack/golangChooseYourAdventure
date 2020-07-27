549e2b169362fbbf51d160503da733da72de5409
Devin Black <Devinjamesblack@gmail.com>
2020-07-26 22:28:43 -0600

    adding styling and ports to the page

--- cyoa/story.go
+++ cyoa/story.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func init() {
@@ -20,15 +22,46 @@ var defaultHanlderTmpl = `
 		<title>Chose Your Own Adventure</title>
 	</head>
 	<body>
-		<h1>{{.Title}}</h1>
-			{{range .Paragraphs}}
-				<p>{{.}}</p>
-			{{end}}
-		<ul>
-			{{range .Options}}
-				<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-			{{end}}
-		</ul>    
+		<div class="context">
+			<h1>{{.Title}}</h1>
+				{{range .Paragraphs}}
+					<p>{{.}}</p>
+				{{end}}
+			<ul>
+				{{range .Options}}
+					<li class="links" ><a href="/{{.Chapter}}">{{.Text}}</a></li>
+				{{end}}
+			</ul>  
+		</div>
+		<style>
+			body {
+				background-color: #708090;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+		
+			.context {
+				max-width: 60rem;
+				margin: auto;
+				padding: 10px;
+				border: 20px solid #6699cc;
+				background-color: #66ccff;
+			}
+		
+			h1 {
+				text-align: center;
+			}
+		
+			a {
+				padding: 1rem;
+				text-decoration: none;
+			}
+		
+			.links {
+				font-size: 1rem;
+			}
+	   </style>
 	</body>
 	</html>
 `
@@ -42,10 +75,23 @@ type handler struct {
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := tpl.Execute(w, h.s["intro"])
-	if err != nil {
-		panic(err)
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+	// "/intro" => "intro"
+	path = path[1:]
+
+	//                   ["intro"]
+	if chapter, ok := h.s[path]; ok {
+		err := tpl.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Chapter not found.", http.StatusNotFound)
 }
 
 func JsonStory(r io.Reader) (Story, error) {
